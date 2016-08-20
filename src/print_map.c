3bd076d59874532398d9f948c05abbbc4f0f5c8a
Cameron de Bruyn <debruyn.sarel@gmail.com>
2016-08-20 14:26:59 +0200

    mapping 2

--- src/print_map.c
+++ src/print_map.c
@@ -5,10 +5,10 @@ void	print_environment(t_env *env)
 	unsigned int	i;
 
 	i = 0;
-	printf("env.ray.start.z:	%f\n", env->r.start.z);
-	printf("env.ray.dir.x:		%f\n", env->r.dir.x);
-	printf("env.ray.dir.y:		%f\n", env->r.dir.y);
-	printf("env.ray.dir.z:		%f\n", env->r.dir.z);
+	printf("env->ray.start.z:		%f\n", env->r.start.z);
+	printf("env->ray.dir.x:			%f\n", env->r.dir.x);
+	printf("env->ray.dir.y:			%f\n", env->r.dir.y);
+	printf("env->ray.dir.z:			%f\n", env->r.dir.z);
 	printf(".\n");
 
 }
@@ -17,13 +17,26 @@ void	print_nodes(t_env *env)
 {
 	while (env->l[2])
 	{
-		printf("l[1].pos.x:		%f\n", env->l[2]->pos.x);
-		printf("l[1].pos.y:		%f\n", env->l[2]->pos.y);
-		printf("l[1].pos.z:		%f\n", env->l[2]->pos.z);
-		printf("l[1].intensity.r:	%f\n", env->l[2]->intensity.r);
-		printf("l[1].intensity.g:	%f\n", env->l[2]->intensity.g);
-		printf("l[1].intensity.b:	%f\n", env->l[2]->intensity.b);
+		printf("env->l[2]->pos.x:		%f\n", env->l[2]->pos.x);
+		printf("env->l[2]->pos.y:		%f\n", env->l[2]->pos.y);
+		printf("env->l[2]->pos.z:		%f\n", env->l[2]->pos.z);
+		printf("env->l[2]->intensity.r:		%f\n", env->l[2]->intensity.r);
+		printf("env->l[2]->intensity.g:		%f\n", env->l[2]->intensity.g);
+		printf("env->l[2]->intensity.b:		%f\n", env->l[2]->intensity.b);
 		printf(".\n");
 		env->l[2] = env->l[2]->next;
+	}
+	while (env->s->sp[2])
+	{
+		printf("env->s->sp[2]->pos.x:		%f\n", env->s->sp[2]->centre.x);
+		printf("env->s->sp[2]->pos.y:		%f\n", env->s->sp[2]->centre.y);
+		printf("env->s->sp[2]->pos.z:		%f\n", env->s->sp[2]->centre.z);
+		printf("env->s->sp[2]->radius:		%f\n", env->s->sp[2]->radius);
+		printf("env->s->sp[2]->m.diffuse.r:	%f\n", env->s->sp[2]->m.diffuse.r);
+		printf("env->s->sp[2]->m.diffuse.g:	%f\n", env->s->sp[2]->m.diffuse.g);
+		printf("env->s->sp[2]->m.diffuse.b:	%f\n", env->s->sp[2]->m.diffuse.b);
+		printf("env->s->sp[2]->m.reflect:	%f\n", env->s->sp[2]->m.reflect);
+		printf(".\n");
+		env->s->sp[2] = env->s->sp[2]->next;		
 	}
 }
--- src/read_map.c
+++ src/read_map.c
@@ -1,5 +1,4 @@
 #include "../includes/rtv1.h"
-#include <stdio.h>
 
 void	read_map(const int fd, t_env *env)
 {
@@ -7,14 +6,14 @@ void	read_map(const int fd, t_env *env)
 
 	get_next_line(fd, &line);
 	if (ft_strcmp(line, "start") == 0)
-	{
 		search_map(fd, line, env);
-	}
 	print_environment(env);
+	print_nodes(env);
 }
 
 void	search_map(const int fd, char *line, t_env *env)
 {
+	env->s = (t_shape *)malloc(sizeof(t_shape *));
 	set_nodes(env);
 	while (get_next_line(fd, &line) != 0)
 	{
@@ -29,27 +28,26 @@ void	search_map(const int fd, char *line, t_env *env)
 			light_list(fd, line, env);
 			continue;
 		}
-//		if (ft_strcmp(line, "OBJECT") == 0)
-//		{
-//			get_shape(fd, line, env);
-//			continue;
-//		}
+		if (ft_strcmp(line, "OBJECT") == 0)
+		{
+			get_shape(fd, line, env);
+			continue;
+		}
 		if (ft_strcmp(line, "end") == 0)
 			break;
 	}
 	reverse_lists(env);
 	create_environment(env);
-	print_nodes(env);
 }
 
 void	create_environment(t_env *env)
 {
 	env->l[2] = env->l[0];
-//	env->s->sp[2] = env->s->sp[0];
+	env->s->sp[2] = env->s->sp[0];
 }
 
 void	set_nodes(t_env *env)
 {
 	env->l[1] = 0;
-//	env->s->sp[1] = 0;
+	env->s->sp[1] = 0;
 }
