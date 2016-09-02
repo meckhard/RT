81a9891a39405da9817325a8d8589028e75f9114
Cameron DE BRUYN <cdebruyn@e5r8p5.jnb.42.fr>
2016-09-02 12:40:26 +0200

    all gone

--- src/ask_commands4.c
+++
@@ -1,8 +0,0 @@
-#include "../includes/rtv1.h"
-
-void	ft_objects(t_objects *ob)
-{
-	ob->objects = (char ****)malloc(sizeof(char ***) * 2);
-	ob->objects[0] = ob->commands;
-	ob->objects[1] = ob->lighting;
-}
